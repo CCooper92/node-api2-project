955677daa0ed2e6e35f66d1bb6bde6147f75b63a
CCooper92 <am10ac17@gmail.com>
2021-04-08 14:10:16 -0700

    trying to deploy

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // require your server and launch it here
+const dotenv = require("dotenv").config()
 const server = require('./api/server');
+const port = process.env.PORT || 9000
 
-server.listen(5000, () => {
-    console.log('server running on localhost 5000')
+
+server.listen(port, () => {
+    console.log(`running on port ${port}`)
 })
